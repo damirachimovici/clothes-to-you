23dec0eeecc5099f1b748c97e7f315df5c48a3bf
damirachimovici <achimovici.damir96@gmail.com>
2021-01-10 17:30:15 +0200

    App-Structure

---
+++ frontend/src/data.js
@@ -0,0 +1,45 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 35,
+      brand: ' Nike',
+      rating: 3.9,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'T-Shirt',
+      category: 'Shirts',
+      image: '/images/d2.jpg',
+      price: 48,
+      brand: 'Pull & Bear',
+      rating: 4.5,
+      numReviews: 7
+    },
+    {
+      _id: '3',
+      name: 'Regular Jeans',
+      category: 'Pants',
+      image: '/images/d3.jpg',
+      price: 99,
+      brand: 'Zara',
+      rating: 4.5,
+      numReviews: 5
+    }, {
+      _id: '4',
+      name: 'Loose Jeans',
+      category: 'Pants',
+      image: '/images/d4.jpg',
+      price: 85,
+      brand: 'New Yorker',
+      rating: 4.9,
+      numReviews: 4
+    },
+  ]
+}
+
+export default data
